aa7fea1a3299d2eecb1ff6072177e583b072e40a
Trent Nielsen <trentnielsen84@gmail.com>
2019-06-28 14:17:13 -0700

    fix list sytanx

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_lb" "default" {
   internal                         = "${var.internal}"
   load_balancer_type               = "application"
   security_groups                  = "${compact(concat(var.security_group_ids, list(aws_security_group.default.id)))}"
-  subnets                          = ["${var.subnet_ids}"]
+  subnets                          = "${var.subnet_ids}"
   enable_cross_zone_load_balancing = "${var.cross_zone_load_balancing_enabled}"
   enable_http2                     = "${var.http2_enabled}"
   idle_timeout                     = "${var.idle_timeout}"
